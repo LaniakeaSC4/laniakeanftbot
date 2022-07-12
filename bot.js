7d68358ecee0fa662c976160ce68963b4dfacfed
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 06:01:51 +0100

    Live test

---
+++ bot.js
@@ -0,0 +1,12 @@
+require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+client.on('message', msg => {
+  msg.reply('pong');
+});
+
+client.login('TOKEN');
