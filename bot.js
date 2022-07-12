04ef592a154bebd296a6006c40372bcb77bb43e8
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 16:06:00 +0100

    Mouse slash command

--- bot.js
+++ bot.js
@@ -5,4 +5,21 @@ client.on('ready', () => {
   console.log(`Logged in....`);
 });
 
+const options = [
+  '🐭',
+  'https://media.giphy.com/media/wJZTbXayokOgbCfyQe/giphy.gif',
+  'https://media.giphy.com/media/QXh9XnIJetPi0/giphy.gif',
+  '🐁',
+];
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+if (interaction.commandName === 'randomice') {
+    await interaction.reply(
+      options[Math.floor(Math.random() * options.length)]
+    );
+  }
+});
+
 client.login(process.env.TOKEN);
