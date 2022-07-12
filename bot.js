25ce7f3ff56f92b9c134606968429ecbb4f5e257
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 16:02:23 +0100

    Register.js

--- bot.js
+++ bot.js
@@ -5,14 +5,4 @@ client.on('ready', () => {
   console.log(`Logged in....`);
 });
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-const data = new SlashCommandBuilder()
-	.setName('echo')
-	.setDescription('Replies with your input!')
-	.addStringOption(option =>
-		option.setName('input')
-			.setDescription('The input to echo back')
-			.setRequired(true));
-
 client.login(process.env.TOKEN);
