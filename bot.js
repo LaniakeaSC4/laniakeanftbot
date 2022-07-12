67ffb4833ae44d5a9fcde30d86413e3a08dfe130
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 09:45:49 +0100

    New intents

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({ allIntents });
+//const Discord = require('discord.js');
+//const client = new Discord.Client({ allIntents });
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
   console.log(`Logged in....`);
