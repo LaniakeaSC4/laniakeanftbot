3864a2c246951b59fe85699d6d4b656373cd0151
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 09:24:15 +0100

    Define new constrat

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 client.on('ready', () => {
   console.log(`Logged in....`);
