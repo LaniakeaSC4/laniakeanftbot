20a7694c5e342eb35476095273824fdd9a9cb2f7
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 13:49:49 +0100

    Slash comand test

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-//const Discord = require('discord.js');
-//const client = new Discord.Client({ allIntents });
-
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -8,8 +5,19 @@ client.on('ready', () => {
   console.log(`Logged in....`);
 });
 
-client.on('message', msg => {
-  msg.reply('pong');
-});
+await rest.put(
+	Routes.applicationCommands(clientId),
+	{ body: commands },
+); 
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder()
+	.setName('echo')
+	.setDescription('Replies with your input!')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('The input to echo back')
+			.setRequired(true));
 
 client.login(process.env.TOKEN);
