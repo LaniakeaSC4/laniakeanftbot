6860361f6ec654670f9c1de4b169fe6e17ebfa07
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 09:38:07 +0100

    Removed intents

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in....`);
