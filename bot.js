12686cfeca7c07a1169ac7526e66144ddec5fc42
LaniakeaSC4 <andy2004@gmail.com>
2022-07-12 18:59:19 +0100

    Different slash attempt

--- bot.js
+++ bot.js
@@ -2,24 +2,36 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
-  console.log(`Logged in....`);
+  console.log(`I'm Ready!`);
 });
 
-const options = [
-  '🐭',
-  'https://media.giphy.com/media/wJZTbXayokOgbCfyQe/giphy.gif',
-  'https://media.giphy.com/media/QXh9XnIJetPi0/giphy.gif',
-  '🐁',
-];
+client.on('ready', () => {
+  client.api.applications(client.user.id).guilds(YOUR_GUILD_ID_HERE).commands.post({
+    data: {
+      name: "hello",
+      description: "hello world command"
+      // possible options here e.g. options: [{...}]
+    }
+  });
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
 
-if (interaction.commandName === 'randomice') {
-    await interaction.reply(
-      options[Math.floor(Math.random() * options.length)]
-    );
-  }
+  client.ws.on('INTERACTION_CREATE', async interaction => {
+    const command = interaction.data.name.toLowerCase();
+    const args = interaction.data.options;
+
+    if (command === 'hello') {
+      // here you could do anything. in this sample
+      // i reply with an api interaction
+      client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: {
+            content: "hello world!!!"
+          }
+        }
+      })
+    }
+  });
 });
 
 client.login(process.env.TOKEN);
