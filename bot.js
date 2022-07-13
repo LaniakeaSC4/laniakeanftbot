bc17525cdb04003e1b1369a80c5fd928254798a2
LaniakeaSC4 <andy2004@gmail.com>
2022-07-13 09:35:18 +0100

    Removed extra code

--- bot.js
+++ bot.js
@@ -55,21 +55,3 @@ client.on('ready', () => {
 client.login(process.env.BOTTOKEN);
 
 
-
-const lib = require('lib')({ token: process.env.STDLIB_SECRET_TOKEN });
-
-await lib.discord.channels['@0.3.0'].messages.create({
-  "channel_id": `${context.params.event.channel_id}`,
-  "content": "",
-  "tts": false,
-  "embeds": [
-    {
-      "type": "rich",
-      "title": `Title example`,
-      "description": `Embed description`,
-      "color": 0xff9d00
-    }
-  ]
-});
-
-
