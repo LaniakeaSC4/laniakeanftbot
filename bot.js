476124fd4fa7dddd6d4df062361f0a0566aaad71
LaniakeaSC4 <andy2004@gmail.com>
2022-07-14 20:39:26 +0100

    No '

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ const puncommon = 0.5;
         console.log('mythic!')
         raritydescription = 'Mythic'
         emoji = '<:mythic:997191007932268555>'
-        embedcolor = '0xed2839';
+        embedcolor = 0xed2839;
         
       } 
       
@@ -92,7 +92,7 @@ const puncommon = 0.5;
         
         console.log('legendary!')
         raritydescription = 'Legendary'
-               embedcolor = '0xed2839';
+               embedcolor = 0xed2839;
         
       } 
       
@@ -100,7 +100,7 @@ const puncommon = 0.5;
       
         console.log('epic!')
         raritydescription = 'Epic'
-               embedcolor = '0xed2839';
+               embedcolor = 0xed2839;
       
       
       }
@@ -109,7 +109,7 @@ const puncommon = 0.5;
       
         console.log('Rare!')
         raritydescription = 'Rare'
-               embedcolor = '0xed2839';
+               embedcolor = 0xed2839;
       
       }
       
@@ -117,7 +117,7 @@ const puncommon = 0.5;
       
         console.log('uncommmon!')
         raritydescription = 'Uncommom'
-               embedcolor = '0xed2839';
+               embedcolor = 0xed2839;
       
       }
       
@@ -125,7 +125,7 @@ const puncommon = 0.5;
       
          console.log('Common')
          raritydescription = 'Common'
-                embedcolor = '0xed2839';
+                embedcolor = 0xed2839;
       
        }
       
@@ -133,7 +133,7 @@ const puncommon = 0.5;
         
         console.log('not ranked')
         raritydescription = 'not ranked'
-               embedcolor = '0xed2839';
+               embedcolor = 0xed2839;
         
       }
       
