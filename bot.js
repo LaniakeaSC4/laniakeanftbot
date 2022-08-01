02e78f913ba566cb36a30ab86a1b9e67e7824453
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-08-01 15:33:09 +0100

    log thisFP

--- bot.js
+++ bot.js
@@ -72,8 +72,10 @@ function getfloorprice(collection) {
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
       console.log('Raw JSON FP: ' + JSON.parse(data).floorPrice)
-      let rawFP = parseFloat(data.floorPrice) / 10000000000//get FP in Sol
+      var rawFP = parseFloat(data.floorPrice)//get FP in Sol
+      var thisFP = rawFP/10000000000
       console.log('rawFP: ' + rawFP)
+      console.log('thisFP: ' + thisFP)
     });
 
   }).on("error", (err) => {
