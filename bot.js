9c6908fab97dc99bf77488b2c5a740bd7f9bcb48
andy2004@gmail.com <andy2004@gmail.com>
2022-08-07 08:47:53 +0100

    Another loop

--- bot.js
+++ bot.js
@@ -361,20 +361,18 @@ async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisemoji
 
 
 async function initalisecollections() {
-  await Promise.all(sequences.map(async value => {
-    await preloadlisting(value)
-  }))
-} 
-
-async function preloadlisting(value){
-  var thisinterval = 1100
+  for (const seq of sequences) {
+    
+    var thisinterval = 1100
   await setTimeout(async function (i) {
     await getnewremotelistings(ourcollections[i][0], initialget).then(async thislistings => {
     ourcollections[i][1] = thislistings//fill tracked listings with the listings we just got
     console.log('added initial ' + initialget + ' Listings for ' + ourcollections[i][0])
     //console.log(listings[0])
   })//end then
-    }, thisinterval, value)
+    }, thisinterval, seq)
+    
+  }
 } 
 
 //main sniper function
