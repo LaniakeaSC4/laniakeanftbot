d72a0a1d0fe1769b43cf9927efb5283a8ad383c0
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-08-08 16:23:41 +0100

    typo

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 const rarityCollections = [
   ['monkeypox_nft', [], 2400],
-  ['pixel_guild_loot_legends', [], 8888]
+  ['pixel_guild_loot_legends', [], 8888],
   ['pokerfaces',[],377]
 ]
 
