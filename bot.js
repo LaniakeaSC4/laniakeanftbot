08c93d470bcc4589f03f9b4503b3039bd3be11f4
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-08-08 08:42:52 +0100

    tidy code

--- bot.js
+++ bot.js
@@ -9,12 +9,12 @@ client.login(process.env.BOTTOKEN)
 //==============================
 
 const rarityCollections = [
-  ['monkeypox_nft',[],2400],
-  ['pixel_guild_loot_legends',[],8888]
-  ]
+  ['monkeypox_nft', [], 2400],
+  ['pixel_guild_loot_legends', [], 8888]
+]
 
 var raritySequencer = []
-for (var i = 0;i < rarityCollections.length;i++) {raritySequencer.push(i)}
+for (var i = 0; i < rarityCollections.length; i++) { raritySequencer.push(i) }
 
 //old vars
 
@@ -62,15 +62,15 @@ const servers = {
 
 //Collections the sniper bot will watch. Must be on moonrank.app
 const sniperCollections = [
-  ['monkeypox_nft',[],2400],
-  ['crypto_coral_tribe',[],6565],
-  ['yeah_tigers',[],10000],
-  ['sphynx_underground_society',[],7777] 
-  ]
+  ['monkeypox_nft', [], 2400],
+  ['crypto_coral_tribe', [], 6565],
+  ['yeah_tigers', [], 10000],
+  ['sphynx_underground_society', [], 7777]
+]
 
 //build array of [0,1,2,etc] for each collection we have. These integers can be used to key access to sniperCollections arrary to loop other functions through all supported collections
 var sniperSequencer = []
-for (var i = 0;i < sniperCollections.length;i++) {sniperSequencer.push(i)}
+for (var i = 0; i < sniperCollections.length; i++) { sniperSequencer.push(i) }
 
 var initialget = 20//how many listings will sniper get initially (max 20)
 var refreshget = 10//how many will sniper get on each check (max 20) - should be less then initial get or extras will count as new
@@ -89,11 +89,11 @@ var mythiclimit = 50
 var legendarylimit = 25
 var epiclimit = 5
 var rarelimit = 2.5
- 
+
 //start sniper bot
 client.on('ready', async () => {
-initaliseSniperCollections() 
-startsniper()
+  initaliseSniperCollections()
+  startsniper()
 })//end client.on Ready
 
 //=================
@@ -104,7 +104,7 @@ startsniper()
 client.on('ready', () => {
 
   console.log('I am ready!')
-  
+
   initaliseRarityCollections()
 
   //enable to reset commands
@@ -117,8 +117,8 @@ async function clearcommands() {
   var serverkeys = Object.keys(servers)
   serverkeys.forEach((key, index) => {
     console.log(servers[key].id)
-  const guild = client.guilds.fetch(servers[key].id)
-  guild.commands.set([])
+    const guild = client.guilds.fetch(servers[key].id)
+    guild.commands.set([])
   })
 }//end function to reset commands
 
@@ -315,7 +315,7 @@ async function getembedcolour(raritydescription) {
   }) //end promise
 }//end testifsnipe function
 
-async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisemoji, thisrarity, raritydescription, thislimit, floorprice, thissnipeprice, thisprice, thisimage,listinglink) {
+async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisemoji, thisrarity, raritydescription, thislimit, floorprice, thissnipeprice, thisprice, thisimage, listinglink) {
   return new Promise((resolve, reject) => {
     client.guilds.cache.get(server).channels.cache.get(snipeschannel).send({
       embeds: [
@@ -338,12 +338,12 @@ async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisemoji
               "name": "Floor Price",
               "value": pround(floorprice, 3) + ' SOL',
               "inline": true
-            }, 
+            },
             {
               "name": "Snipe Price",
-              "value": 'For ' + raritydescription + ' NFTs, any price less than ' + thislimit + 'x the floor price of ' + pround(floorprice,3) + ' SOL is a snipe (i.e. less than ' + pround(thissnipeprice, 3) + ' SOL)',
+              "value": 'For ' + raritydescription + ' NFTs, any price less than ' + thislimit + 'x the floor price of ' + pround(floorprice, 3) + ' SOL is a snipe (i.e. less than ' + pround(thissnipeprice, 3) + ' SOL)',
               "inline": true
-            } 
+            }
           ],
           "image": {
             "url": thisimage,
@@ -361,178 +361,175 @@ async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisemoji
 
 const initaliseSniperCollections = async () => {
   for (const seq of sniperSequencer) {//for each collection
-  //get initial set of listings and store them in the local history arrary for that collection
+    //get initial set of listings and store them in the local history arrary for that collection
     await getnewremoteMElistings(sniperCollections[seq][0], initialget).then(async thislistings => {
-    sniperCollections[seq][1] = thislistings//fill tracked listings with the listings we just got
-    console.log('added initial ' + initialget + ' Listings for ' + sniperCollections[seq][0])
-    }) 
+      sniperCollections[seq][1] = thislistings//fill tracked listings with the listings we just got
+      console.log('added initial ' + initialget + ' Listings for ' + sniperCollections[seq][0])
+    })
     await wait(2000)//add delay between API requests
   }//for seq of sniperSequencer
 }//end initaliseSniperCollections
 
 //main sniper function
 async function startsniper() {
-  
+
   await Promise.all(sniperSequencer.map(async value => {
-    var thisinterval = the_interval + (value*1100)//interval for each collection is 1.1 seconds longer to avoid more than 2 ME API requests per second
-    console.log('Initialising loop for collection: ' + value + '. Setting interval to: ' + thisinterval)
+    var thisinterval = the_interval + (value * 1100)//interval for each collection is 1.1 seconds longer to avoid more than 2 ME API requests per second
+    console.log('Initialising recheckloop for collection: ' + value + '. Setting interval for this collection to: ' + thisinterval)
 
-  await setInterval(async function (k) {//do this every X minutes
+    await setInterval(async function (k) {//do this every X minutes
 
-    await getnewremoteMElistings(sniperCollections[k][0], refreshget).then(async thislistings => {//get latest X listings from Magic Eden
+      await getnewremoteMElistings(sniperCollections[k][0], refreshget).then(async thislistings => {//get latest X listings from Magic Eden
 
-      console.log("I am doing my " + minutes + " minute check for " + sniperCollections[k][0] + '. I have this many in my history at start: ' + sniperCollections[k][1].length)
+        console.log("I am doing my " + minutes + " minute check for " + sniperCollections[k][0] + '. I have this many in my history at start: ' + sniperCollections[k][1].length)
 
-      var rebuildarrary = sniperCollections[k][1]//save all the acquired listings in a temporary arrary
+        var rebuildarrary = sniperCollections[k][1]//save all the acquired listings in a temporary arrary
 
-      for (var i = 0; i < thislistings.length; i++) {//for all listings recieved from getnewremoteMElistingsfunction
+        for (var i = 0; i < thislistings.length; i++) {//for all listings recieved from getnewremoteMElistingsfunction
 
-        if (sniperCollections[k][1].some(e => (e.tokenAddress === thislistings[i].tokenAddress && e.price === thislistings[i].price))) {
-          //actions if token address and price match (i.e. we've seen this one before)
-          //console.log('matched ' + thislistings[i].tokenAddress + ' at price ' + thislistings[i].price)
+          if (sniperCollections[k][1].some(e => (e.tokenAddress === thislistings[i].tokenAddress && e.price === thislistings[i].price))) {
+            //actions if token address and price match (i.e. we've seen this one before)
 
-        } else {
-          //actions if token address or price does not match one we have seen before
-          console.log('New/updated ' + sniperCollections[k][0] + ' entry ' + thislistings[i].tokenAddress + ' at price ' + thislistings[i].price)
-          rebuildarrary.unshift(thislistings[i])//add the new entry to the start of the rebuild arrary so we can remember this one if we see it later
-          
-          console.log(thislistings[i])
+          } else {
+            //actions if token address or price does not match one we have seen before
+            console.log('New/updated ' + sniperCollections[k][0] + ' entry ' + thislistings[i].tokenAddress + ' at price ' + thislistings[i].price)
+            rebuildarrary.unshift(thislistings[i])//add the new entry to the start of the rebuild arrary so we can remember this one if we see it later
 
-          //set price of this lisitng
-          var thistoken = {}
-          var thisprice = pround(thislistings[i].price, 6)
-          var thisname = ''
-          var thisnftid = ''
-          var thisrarity = ''
-          var thisranges = []
-          var thisraritydescription = ''
-          var thisfloorprice = 0
-          var thissnipe = ''
-          var thisembedcolour = ''
-          var thissnipeprice = 0
-          var thislimit = 0
-          var thisimage = ''
-          var thislistinglink = ''
+            //set price of this lisitng
+            var thistoken = {}
+            var thisprice = pround(thislistings[i].price, 6)
+            var thisname = ''
+            var thisnftid = ''
+            var thisrarity = ''
+            var thisranges = []
+            var thisraritydescription = ''
+            var thisfloorprice = 0
+            var thissnipe = ''
+            var thisembedcolour = ''
+            var thissnipeprice = 0
+            var thislimit = 0
+            var thisimage = ''
+            var thislistinglink = ''
 
-          getremotetokendetails(thislistings[i].tokenMint)
-            .then((recievedtoken) => {
-              
-              thistoken = recievedtoken
-              thisname = thistoken.name
-              thisimage = thistoken.image
-              thislistinglink = 'https://magiceden.io/item-details/' + thistoken.mintAddress
-              
-              //get nft ID
-              let namearr = thistoken.name.split(' ')
-              for (var i = 0; i < namearr.length; i++) {
-                let checkthis = namearr[i]
-                if (checkthis.includes('#')) {
+            getremotetokendetails(thislistings[i].tokenMint)
+              .then((recievedtoken) => {
 
-                  var nlength = checkthis.length
-                  thisnftid = checkthis.substring(1, nlength)
+                thistoken = recievedtoken
+                thisname = thistoken.name
+                thisimage = thistoken.image
+                thislistinglink = 'https://magiceden.io/item-details/' + thistoken.mintAddress
 
-                }//end if
-              }//end for
+                //get nft ID
+                let namearr = thistoken.name.split(' ')
+                for (var i = 0; i < namearr.length; i++) {
+                  let checkthis = namearr[i]
+                  if (checkthis.includes('#')) {
 
-              //get rarity
-              for (var i = 0; i < sniperCollections[k][1].length; i++) {
+                    var nlength = checkthis.length
+                    thisnftid = checkthis.substring(1, nlength)
 
-                if (thistoken.mintAddress == sniperCollections[k][1][i].tokenMint) {
-                  thisrarity = sniperCollections[k][1][i].rarity.moonrank.rank//end moonrank data from ME
-                  break
+                  }//end if
+                }//end for
+
+                //get rarity
+                for (var i = 0; i < sniperCollections[k][1].length; i++) {
+
+                  if (thistoken.mintAddress == sniperCollections[k][1][i].tokenMint) {
+                    thisrarity = sniperCollections[k][1][i].rarity.moonrank.rank//end moonrank data from ME
+                    break
+                  }
                 }
-              }
 
-              return calculateranges(sniperCollections[k][2])
+                return calculateranges(sniperCollections[k][2])
 
-            })//end .then
-            .then((ranges) => {
+              })//end .then
+              .then((ranges) => {
 
-              var mythicstart = ranges[0]; var mythicend = ranges[1]
-              var legendarystart = ranges[2]; var legendaryend = ranges[3]
-              var epicstart = ranges[4]; var epicend = ranges[5]
-              var rarestart = ranges[6]; var rareend = ranges[7]
-              var uncommonstart = ranges[8]; var uncommonend = ranges[9]
-              var commonstart = ranges[10]; var commonend = ranges[11]
+                var mythicstart = ranges[0]; var mythicend = ranges[1]
+                var legendarystart = ranges[2]; var legendaryend = ranges[3]
+                var epicstart = ranges[4]; var epicend = ranges[5]
+                var rarestart = ranges[6]; var rareend = ranges[7]
+                var uncommonstart = ranges[8]; var uncommonend = ranges[9]
+                var commonstart = ranges[10]; var commonend = ranges[11]
 
-              thisranges = ranges//store outside subsection so we can access it
-              
-              return getraritydescription(mythicstart, mythicend, legendarystart, legendaryend, epicstart, epicend, rarestart, rareend, uncommonstart, uncommonend, commonstart, commonend, thisrarity)
-            })//end .then
-            .then((raritydescription) => {
+                thisranges = ranges//store outside subsection so we can access it
 
-              thisraritydescription = raritydescription//store outside subsection so we can access it
+                return getraritydescription(mythicstart, mythicend, legendarystart, legendaryend, epicstart, epicend, rarestart, rareend, uncommonstart, uncommonend, commonstart, commonend, thisrarity)
+              })//end .then
+              .then((raritydescription) => {
 
-              return getremotefloorprice(sniperCollections[k][0])
-            })//end .then
-            .then((floorprice) => {
+                thisraritydescription = raritydescription//store outside subsection so we can access it
 
-              thisfloorprice = pround(floorprice, 6)//store outside subsection so we can access it
+                return getremotefloorprice(sniperCollections[k][0])
+              })//end .then
+              .then((floorprice) => {
 
-              return testifsnipe(thisraritydescription, thisprice, thisfloorprice)
-            })//end .then
-            .then((snipe) => {
+                thisfloorprice = pround(floorprice, 6)//store outside subsection so we can access it
 
-//store outside subsection so we can access it
-              thissnipe = snipe[0]
-              thissnipeprice = snipe[1]
-              thislimit = snipe[2]
+                return testifsnipe(thisraritydescription, thisprice, thisfloorprice)
+              })//end .then
+              .then((snipe) => {
 
-              if (thissnipe != "false") {
-                console.log('we have a ' + sniperCollections[k][0] + ' snipe!')
-                return getembedcolour(thisraritydescription)
-              }
-            })//end .then
-            .then((embedcolour) => {
+                //store outside subsection so we can access it
+                thissnipe = snipe[0]
+                thissnipeprice = snipe[1]
+                thislimit = snipe[2]
 
-              thisembedcolour = embedcolour//store outside subsection so we can access it
-              var thisserver = ''
-              var thisserverid = ''
-              var thissnipechannel = ''
-              var thisemoji = ''
+                if (thissnipe != "false") {
+                  console.log('we have a ' + sniperCollections[k][0] + ' snipe!')
+                  return getembedcolour(thisraritydescription)
+                }
+              })//end .then
+              .then((embedcolour) => {
 
-              if (thissnipe != "false") {//if this is a snipe get emoji and send messages out to each server
-                var serverkeys = Object.keys(servers)
-                serverkeys.forEach((key, index) => {//for each server
+                thisembedcolour = embedcolour//store outside subsection so we can access it
+                var thisserver = ''
+                var thisserverid = ''
+                var thissnipechannel = ''
+                var thisemoji = ''
 
-                  //get the snipes channel id from the servers config object
-                  var emojis = Object.keys(servers[key].emoji)
-                  thisserver = servers[key]
-                  thisserverid = servers[key].id
-                  thissnipechannel = servers[key].snipeschannel
+                if (thissnipe != "false") {//if this is a snipe get emoji and send messages out to each server
+                  var serverkeys = Object.keys(servers)
+                  serverkeys.forEach((key, index) => {//for each server
 
-                  emojis.forEach((key, index) => {//loop through each potential emoji
-                    if (key === thisraritydescription) { thisemoji = thisserver.emoji[key] }//end if key matches emoji we are looking for
-                  })//end for each potential emoji loop
+                    //get the snipes channel id from the servers config object
+                    var emojis = Object.keys(servers[key].emoji)
+                    thisserver = servers[key]
+                    thisserverid = servers[key].id
+                    thissnipechannel = servers[key].snipeschannel
 
-                  //send snipes
-                  sendsnipes(thisserverid,thissnipechannel,thisname,thisembedcolour,thisemoji,thisrarity,thisraritydescription,thislimit,thisfloorprice,thissnipeprice,thisprice,thisimage,thislistinglink)
+                    emojis.forEach((key, index) => {//loop through each potential emoji
+                      if (key === thisraritydescription) { thisemoji = thisserver.emoji[key] }//end if key matches emoji we are looking for
+                    })//end for each potential emoji loop
 
-                })//end for each server
-              }//end if this is a snipe
-            })//end .then
+                    //send snipes
+                    sendsnipes(thisserverid, thissnipechannel, thisname, thisembedcolour, thisemoji, thisrarity, thisraritydescription, thislimit, thisfloorprice, thissnipeprice, thisprice, thisimage, thislistinglink)
 
-        }//end else for a token we havnt seen before
+                  })//end for each server
+                }//end if this is a snipe
+              })//end .then
 
-      }//end for loop of each listing recieved
+          }//end else for a token we havnt seen before
 
-      //for each collection we store a max history. Clear the oldest ones if it's longer than that. 
-      if (rebuildarrary.length > maxlength) {
-        var numbertoremove = rebuildarrary.length - maxlength
-        console.log('number to remove is: ' + numbertoremove)
-        for (var i = 0; i < numbertoremove; i++) {
-          console.log("1 removal loop - popping here")
-          rebuildarrary.pop()//remove oldest entry
-        }//end for number to remove
-      }//end if rebuildarrary is longer than max length
+        }//end for loop of each listing recieved
 
-      sniperCollections[k][1] = rebuildarrary//overwrite main listings arrary with the temp rebuild one
+        //for each collection we store a max history. Clear the oldest ones if it's longer than that. 
+        if (rebuildarrary.length > maxlength) {
+          var numbertoremove = rebuildarrary.length - maxlength
+          console.log('number to remove is: ' + numbertoremove)
+          for (var i = 0; i < numbertoremove; i++) {
+            console.log("1 removal loop - popping here")
+            rebuildarrary.pop()//remove oldest entry
+          }//end for number to remove
+        }//end if rebuildarrary is longer than max length
 
-    })//end then after getting 
+        sniperCollections[k][1] = rebuildarrary//overwrite main listings arrary with the temp rebuild one
 
-  }, thisinterval, value)//end recheck listing loop
+      })//end then after getting 
+
+    }, thisinterval, value)//end recheck listing loop
   })
-)
+  )
 }//end startsniper
 
 //=========================
