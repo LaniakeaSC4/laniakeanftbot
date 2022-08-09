c534e5201c565d6a56e6ca88d0c65adab5f3e335
andy2004@gmail.com <andy2004@gmail.com>
2022-08-09 15:55:24 +0100

    On conflict do nothing

--- bot.js
+++ bot.js
@@ -626,7 +626,7 @@ client.ws.on('INTERACTION_CREATE', async interaction => {
           if (thisdata.result.api_code === 200) {
             console.log('Recieved collection: ' + thisdata.result.data.collection + 'from howrare.is with status code:' + thisdata.result.api_code + '. Ready to add to SQL')
 
-            var querystring = 'INSERT INTO howraredata( collection_ID, data, created_on, last_updated ) VALUES ( $1,$2,to_timestamp($3 / 1000.0),to_timestamp($4 / 1000.0) )'
+            var querystring = 'INSERT INTO howraredata( collection_ID, data, created_on, last_updated ) VALUES ( $1,$2,to_timestamp($3 / 1000.0),to_timestamp($4 / 1000.0) ) ON CONFLICT ON CONSTRAINT collection_ID DO NOTHING'
             var querydata = [collectionstring, thisdata, Date.now(), Date.now()]
 
             await pgclient.query(querystring, querydata, (err, res) => {
