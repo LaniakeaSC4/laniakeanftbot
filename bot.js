9b1c45f344f0b45064907cf45c90752e43db6c14
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-08-09 08:49:58 +0100

    member.user.id

--- bot.js
+++ bot.js
@@ -605,9 +605,9 @@ client.on('ready', () => {
 client.ws.on('INTERACTION_CREATE', async interaction => {
   const command = interaction.data.name.toLowerCase()
   const args = interaction.data.options//array of the provided data after the slash
-console.log(interaction)
+
   if (command === 'database') {
-    if (interaction.user.id === "684896787655557216") {
+    if (interaction.member.user.id === "684896787655557216") {
       client.api.interactions(interaction.id, interaction.token).callback.post({
         data: {
           type: 4,
