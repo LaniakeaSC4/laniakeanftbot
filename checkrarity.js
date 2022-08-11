430c65a25a941926289363910df8e2873016029c
andy2004@gmail.com <andy2004@gmail.com>
2022-08-11 07:21:00 +0100

    Log

--- checkrarity.js
+++ checkrarity.js
@@ -9,6 +9,7 @@ async function getPostgresCollectionSize(collectionID) {
     var querystring = "SELECT COUNT(*) FROM (SELECT jsonb_path_query(data, '$.result.data.items[*]') FROM howraredata WHERE collection_id = '" + collectionID + "') AS nftcount"
     pgclient.query(querystring, (err, res) => {
       if (err) throw err
+      console.log('I am in another file')
       resolve(res.rows[0].count)
     })//end query
   })//end promise
