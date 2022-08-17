fb34c7c212327c45728814aeadcf287ce4b9c293
andy2004@gmail.com <andy2004@gmail.com>
2022-08-17 19:13:54 +0100

    Just 0

--- functions/postgres.js
+++ functions/postgres.js
@@ -149,8 +149,8 @@ async function getData(table, tableprimarykey, thisprimarykey, column) {
 
     pgclient.query(querystring, (err, res) => {
       if (err) throw err
-      console.log(res.rows[0][column])
-      resolve(res.rows[0][column])
+      console.log(res.rows[0][column][0])
+      resolve(res.rows[0][column][0])
     })
   })
 };module.exports.getData = getData
