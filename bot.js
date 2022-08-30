71dc5e969f2c860ac72bb291f9715048eaaafc69
andy2004@gmail.com <andy2004@gmail.com>
2022-08-30 18:30:55 +0100

    Winston.logger

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require('dotenv').config()//import process environment vars into app engine node
 const fs = require('node:fs')
 const path = require('node:path')
 
-const logger = require('./tools/winston.js')
+const winston = require('./tools/winston.js')
 
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const client = new Client({
@@ -82,8 +82,8 @@ const servers = {
 client.on('ready', async () => {
   console.log('I am ready!')
   // Writes some log entries
-  logger.error('warp nacelles offline');
-  logger.info('shields at 99%');
+  winston.logger.error('warp nacelles offline');
+  winston.logger.info('shields at 99%');
   sniperv2.initialise()
 
   //endable/disable sniper v1
