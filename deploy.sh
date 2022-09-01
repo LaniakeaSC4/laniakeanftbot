eb82ae5dbfb3f9af2065bbc427cc52311e0182cc
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-09-01 11:53:45 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-#stop
-forever stopall
+# stopall
+echo stopping processes
+forever stopall &
+wait
 #sync repo
-gh repo sync
-#restart
-forever start bot.js
+echo syncing github
+gh repo sync &
+wait
+#go to directory
+echo changing directory
+cd /home/abwglv/nftbot/ &
+wait
+#start bot
+echo starting bot
+forever start bot.js &
+wait
+#delete the logs from last instance of process
+echo deleting logs
+forever cleanlogs
