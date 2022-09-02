b46a7dafe48996272b8e34871d264c8e6eea9744
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-09-02 10:28:01 +0100

    tostring for log

--- commands/rarity.js
+++ commands/rarity.js
@@ -29,9 +29,9 @@ module.exports = {
     //get the inputs from the command
     var collectionKey = interaction.options.getString('collectionkey'); var nftid = interaction.options.getString('nftid')
     var supportedcollections = await sql.getOurMetaplexCollections()//set supported collections from sql
-    w.log.info('collection key is; ' + collectionKey)
+    w.log.info('collection key is; ' + collectionKey.toString())
     w.log.info('supported collections are: ')
-    w.log.info(supportedcollections)
+    w.log.info(supportedcollections.toString())
     if (supportedcollections.includes(collectionKey)) {//check if the user has typed a valid collection in the database
       rarityembed = await raritychecker.check(collectionKey, nftid)//check rarity
       if (rarityembed) {//if an embed is returned, reply with it
