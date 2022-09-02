9af0c6642177bdbd4ffe3179d7cb77c69a919ca3
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-09-02 14:48:52 +0100

    import env variabled into deploy commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,18 +3,16 @@ const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const w = require('./tools/winston.js')
-
-//const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config()//import process environment vars into app engine nodejs environment using dotenv
 
 const sql = require('./tools/commonSQL.js')//common sql related commands are in here
 
 const getservers = async () => {
   const servers = await sql.getRowsForColumn('servers', 'serverid')
-return servers
+  return servers
 }
 
-
-const clientId = '996170261353222219' 
+const clientId = '996170261353222219'
 const guildId = '978975057739124767'
 
 const commands = [];
@@ -22,22 +20,21 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJSON());
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOTTOKEN);
- 
 
- getservers().then(servers => {
-   for (var i = 0;i < servers.length;i++) {
+getservers().then(servers => {
+  for (var i = 0; i < servers.length; i++) {
     rest.put(Routes.applicationGuildCommands(clientId, servers[i].serverid), { body: commands })
-    .then(() => w.log.info('Successfully registered application commands'))
-    .catch(console.error);
-    
-}  
-   
-   
- })
- 
+      .then(() => w.log.info('Successfully registered application commands'))
+      .catch(console.error);
+
+  }
+
+
+})
+
