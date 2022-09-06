85012d1c7b7f2b5ee5305fc6669ce00081522f3a
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-09-06 15:46:07 +0100

    reformat bot post

--- sniper/v2/sniper-v2.js
+++ sniper/v2/sniper-v2.js
@@ -28,8 +28,8 @@ var supportedservers = []
 
 const initaliseSniperCollections = async () => {
   //reset globals incase this is a restart
-  supportedservers = [];sniperSequencer = [];collections = {}
-  
+  supportedservers = []; sniperSequencer = []; collections = {}
+
   collections = await sql.getSupportedCollections()
 
   var currentcollections = ""
@@ -47,7 +47,7 @@ const initaliseSniperCollections = async () => {
 
   //get servers and load into supported servers var
   supportedservers = await sql.getSupportedServers()
-  
+
   startsniper()
 }//end initaliseSniperCollections
 module.exports.initialise = initaliseSniperCollections
@@ -62,7 +62,7 @@ async function startsniper() {
     var thisinterval = the_interval + (value * 1100)//interval for each collection is 1.1 seconds longer to avoid more than 2 ME API requests per second
     //w.log.info('SniperV2: Initialising recheckloop for collection: ' + collections[value].collectionkey + '. Setting interval for this collection to: ' + thisinterval)
 
-   var thisintervalid = await setInterval(async function (k) {//do this every X minutes
+    var thisintervalid = await setInterval(async function (k) {//do this every X minutes
       await magiceden.getNewListings(collections[k]['meslug'], refreshget).then(async thislistings => {//get latest X listings from Magic Eden
         /* heartbeat logging - enable if you want update each minute for each collection */
         //w.log.info("I am doing my " + minutes + " minute check for " + sniperCollections[k][0] + '. I have this many in my history at start: ' + sniperCollections[k][1].length)
@@ -80,63 +80,65 @@ async function startsniper() {
 
             var thisprice = pround(thislistings[i].price, 6)//set price of this lisitng
             var recievedtoken = await magiceden.getTokenDetails(thislistings[i].tokenMint)
-            
+
             if (recievedtoken) {
 
-            var thistoken = recievedtoken
-            var thisname = thistoken.name
-            var thisimage = thistoken.image
-            var thislistinglink = 'https://magiceden.io/item-details/' + thistoken.mintAddress
+              var thistoken = recievedtoken
+              var thisname = thistoken.name
+              var thisimage = thistoken.image
+              var thislistinglink = 'https://magiceden.io/item-details/' + thistoken.mintAddress
 
-            //get nft from nane
-            var thisnftid = ''
+              //get nft from nane
+              var thisnftid = ''
 
-            let namearr = thistoken.name.split(' ')
-            for (var i = 0; i < namearr.length; i++) {
-              let checkthis = namearr[i]
-              if (checkthis.includes('#')) {
-                var nlength = checkthis.length
-                thisnftid = checkthis.substring(1, nlength)
-              }//end if
-            }//end for
+              let namearr = thistoken.name.split(' ')
+              for (var i = 0; i < namearr.length; i++) {
+                let checkthis = namearr[i]
+                if (checkthis.includes('#')) {
+                  var nlength = checkthis.length
+                  thisnftid = checkthis.substring(1, nlength)
+                }//end if
+              }//end for
 
-            var NFTdata = await sql.getNFTdata(collections[k]['collectionkey'], thisnftid)
-            if (NFTdata) {
-              var collectionSize = await sql.getData("solanametaplex", "collectionkey", collections[k]['collectionkey'], 'collectioncount')
-              var raritydescription = await nfttools.getraritydescription(collectionSize, NFTdata.rarityRank)
-              var thisembedcolour = await nfttools.getembedcolour(raritydescription)
-              var floorprice = await magiceden.getFloorPrice(collections[k]['meslug'])
-              var thisfloorprice = pround(parseFloat(floorprice), 6)
-              var snipe = await testifsnipe(raritydescription, parseFloat(thisprice), parseFloat(thisfloorprice))
+              var NFTdata = await sql.getNFTdata(collections[k]['collectionkey'], thisnftid)
+              if (NFTdata) {
+                var collectionSize = await sql.getData("solanametaplex", "collectionkey", collections[k]['collectionkey'], 'collectioncount')
+                var raritydescription = await nfttools.getraritydescription(collectionSize, NFTdata.rarityRank)
+                var thisembedcolour = await nfttools.getembedcolour(raritydescription)
+                var floorprice = await magiceden.getFloorPrice(collections[k]['meslug'])
+                var thisfloorprice = pround(parseFloat(floorprice), 6)
+                var snipe = await testifsnipe(raritydescription, parseFloat(thisprice), parseFloat(thisfloorprice))
 
-              if (snipe != "false") {
-                var thissnipeprice = parseFloat(snipe[1])
-                var thislimit = parseFloat(snipe[2])
-                var hotness = await snipeHotness(parseFloat(thisprice), thisfloorprice, parseFloat(thissnipeprice))
-                var thisserverid = ''
-                var thissnipechannel = ''
+                if (snipe != "false") {
+                  var thissnipeprice = parseFloat(snipe[1])
+                  var thislimit = parseFloat(snipe[2])
+                  var hotness = await snipeHotness(parseFloat(thisprice), thisfloorprice, parseFloat(thissnipeprice))
+                  var thisserverid = ''
+                  var thissnipechannel = ''
 
-                w.log.info('SniperV2: we have a ' +  collections[k]['meslug'] + ' snipe!')
+                  w.log.info('SniperV2: we have a ' + collections[k]['meslug'] + ' snipe!')
 
-                for (i = 0; i < supportedservers.length; i++) {
+                  for (i = 0; i < supportedservers.length; i++) {
 
-                  //get the snipes channel id to send the snipe to
-                  thisserverid = supportedservers[i].serverid
-                  if (raritydescription === 'Rare') { thissnipechannel = supportedservers[i].raresnipes }
-                  if (raritydescription === 'Epic') { thissnipechannel = supportedservers[i].epicsnipes }
-                  if (raritydescription === 'Legendary') { thissnipechannel = supportedservers[i].legendarysnipes }
-                  if (raritydescription === 'Mythic') { thissnipechannel = supportedservers[i].mythicsnipes }
-if (thissnipechannel){//filters out servers which are in pg but not setup yet
-                  //send snipes
-                  sendsnipes(thisserverid, thissnipechannel, thisname, thisembedcolour, NFTdata.rarityRank, raritydescription, thislimit, thisfloorprice, thissnipeprice, thisprice, thisimage, thislistinglink, hotness)
-}//end if snipe channel.
-                }//for each supported server (from SQL)                
+                    //get the snipes channel id to send the snipe to
+                    thisserverid = supportedservers[i].serverid
+                    if (raritydescription === 'Rare') { thissnipechannel = supportedservers[i].raresnipes }
+                    if (raritydescription === 'Epic') { thissnipechannel = supportedservers[i].epicsnipes }
+                    if (raritydescription === 'Legendary') { thissnipechannel = supportedservers[i].legendarysnipes }
+                    if (raritydescription === 'Mythic') { thissnipechannel = supportedservers[i].mythicsnipes }
 
-              } else { /* w.log.info('this was not a snipe') */ } //end if not false
-            } else {
-              w.log.error('error getting nft data')
-            }//end else if get nft data failed
-            } else {w.log.error('error getting listing at magic Eden for this snipe test')}
+                    if (thissnipechannel) {//filters out servers which are in pg but not setup yet by checking if the snipe channel is valid for this server
+                      //send snipes
+                      sendsnipes(thisserverid, thissnipechannel, thisname, thisembedcolour, NFTdata.rarityRank, raritydescription, thislimit, thisfloorprice, thissnipeprice, thisprice, thisimage, thislistinglink, hotness)
+                    }//end if snipe channel.
+
+                  }//for each supported server (from SQL)                
+
+                } else { /* w.log.info('this was not a snipe') */ } //end if not false
+              } else {
+                w.log.error('error getting nft data')
+              }//end else if get nft data failed
+            } else { w.log.error('error getting listing at magic Eden for this snipe test') }
           }//end else for a token we havnt seen before
         }//end for loop of each listing recieved
 
@@ -153,8 +155,8 @@ if (thissnipechannel){//filters out servers which are in pg but not setup yet
 
       })//end then after getting 
     }, thisinterval, value)//end recheck listing loop
-  //save this interval id so we can kill it in a restart
-  currentloops.push(thisintervalid)
+    //save this interval id so we can kill it in a restart
+    currentloops.push(thisintervalid)
   })//end snipersequencer values
   )//end promise.all
 }//end startsniper
@@ -167,11 +169,11 @@ async function snipeHotness(thisprice, floorprice, thislimit) {
   var warmlimit = ((thislimit - floorprice) * 0.8); //w.log.info('warmlimit limit is: ' + warmlimit)
   var coollimit = thislimit; //w.log.info('coollimit limit is: ' + coollimit)
 
-  if (thisprice <= blazinglimit) { return '🔥🔥🔥🔥🔥\nBlazing Hot' }
-  if (thisprice <= redhotlimit && thisprice > blazinglimit) { return '🔥🔥🔥🔥\nRed Hot' }
-  if (thisprice <= hotlimit && thisprice > redhotlimit) { return '🔥🔥🔥\nHot' }
-  if (thisprice <= warmlimit && thisprice > hotlimit) { return '🔥🔥\nWarm' }
-  if (thisprice <= coollimit && thisprice > warmlimit) { return '🔥\nCool' }
+  if (thisprice <= blazinglimit) { return '🔥🔥🔥🔥🔥\n__Blazing Hot' }
+  if (thisprice <= redhotlimit && thisprice > blazinglimit) { return '🔥🔥🔥🔥\n__Red Hot' }
+  if (thisprice <= hotlimit && thisprice > redhotlimit) { return '🔥🔥🔥\n__Hot' }
+  if (thisprice <= warmlimit && thisprice > hotlimit) { return '🔥🔥\n__Warm' }
+  if (thisprice <= coollimit && thisprice > warmlimit) { return '🔥\n__Cool' }
 }//end fnction snipeHotness
 
 async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisrarity, raritydescription, thislimit, floorprice, thissnipeprice, thisprice, thisimage, listinglink, hotness) {
@@ -179,22 +181,22 @@ async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisrarit
     client.guilds.cache.get(server).channels.cache.get(snipeschannel).send({
       embeds: [
         {
-          "title": hotness + ' Snipe Opportunity\n\n' + nftname,
+          "title": hotness + ' Snipe Opportunity__\n' + nftname,
           "color": embedcolour,
           "fields": [
             {
-              "name": "🎯 - Snipe Details",
-              "value": "**Rarity**: " + thisrarity + ' - ' + raritydescription + "\n**List price**: " + pround(parseFloat(thisprice), 3) + ' SOL\n**Floor price**: ' + pround(parseFloat(floorprice), 3) + ' SOL\n[Buy on Magic Eden]('+listinglink+')' ,
+              "name": "🎯 ▪ __Snipe Details__",
+              "value": "**Rarity**: " + thisrarity + ' - ' + raritydescription + "\n**List price**: " + pround(parseFloat(thisprice), 3) + ' SOL\n**Floor price**: ' + pround(parseFloat(floorprice), 3) + ' SOL\n[Buy on Magic Eden](' + listinglink + ')\n',
               "inline": false
             },
             {
-              "name": "⚡ - Snipe Price Info",
+              "name": "⚡ ▪ __Snipe Price Info__",
               "value": 'For ' + raritydescription + ' NFTs, any price less than ' + parseFloat(thislimit) + 'x the floor price of ' + pround(parseFloat(floorprice), 3) + ' SOL is a snipe (i.e. less than ' + pround(parseFloat(thissnipeprice), 3) + ' SOL)',
               "inline": true
             },
-             {
-              "name": "🐍 - Laniakea Bot",
-              "value": "Hand crafted by Laniakea#3683 and brought to you by the Secret Snake Society. Visit us [on Discord](https://discord.gg/Dr2Vb7Rt6u)",
+            {
+              "name": "🐍 ▪ __Laniakea Bot v2.1__",
+              "value": "Hand crafted by Laniakea#3683 and brought to you by the Secret Snake Society. Visit us [on Discord](https://discord.gg/Dr2Vb7Rt6u).",
               "inline": false
             }
           ],
@@ -202,9 +204,6 @@ async function sendsnipes(server, snipeschannel, nftname, embedcolour, thisrarit
             "url": thisimage,
             "height": 75,
             "width": 75
-          },
-          "footer": {
-            "text": "Sniper Bot v2.1 and Rarity data by Laniakea#3683"
           }
         }
       ]//end embed
@@ -242,9 +241,9 @@ async function testifsnipe(raritydescription, thisprice, floorprice) {
   }) //end promise
 }//end testifsnipe function
 
-async function stopsniper(loops){
+async function stopsniper(loops) {
   w.log.info('stopping sniper bot with clearinterval')
-  for (var i = 0;i < currentloops.length;i++){
+  for (var i = 0; i < currentloops.length; i++) {
     clearInterval(currentloops[i])
   }
   currentloops = []//reset it
