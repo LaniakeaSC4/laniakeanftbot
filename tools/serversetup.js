338688ef4fcc4b614190f4797aafe02e1e6c71ff
andy2004@gmail.com <andy2004@gmail.com>
2022-09-07 20:07:58 +0100

    Edit reply

--- tools/serversetup.js
+++ tools/serversetup.js
@@ -339,5 +339,5 @@ async function homechannelsetup3(interaction) {
 	const response = interaction.fields.getTextInputValue('collection-input')
 	var meslug = response.substring(response.lastIndexOf('magiceden.io/marketplace/') + 25).replace(/[^0-9a-z]/gi, '')
 	homecollections.enabled.push(meslug)
-	interaction.edit({ content: "Adding: " + homecollections.enabled.toString(), ephemeral: true })
+	interaction.editReply({ content: "Adding: " + homecollections.enabled.toString(), ephemeral: true })
 } module.exports.homechannelsetup3 = homechannelsetup3
