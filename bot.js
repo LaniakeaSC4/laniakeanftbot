c41785ee7536b48ca807b6d41880ae556e3cf195
andy2004@gmail.com <andy2004@gmail.com>
2022-09-07 07:01:30 +0100

    Remove button return

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ const sql = require('./tools/commonSQL.js')
 
 
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isButton()) return;
+  
   if (interaction.customId === 'beginsetup') {
     var setupstatus = await setup.start(interaction)//creates category and 4 sniper channels if the ones in database dont already exist.
     if (setupstatus) { w.log.info('setup status was sucessful') } else {w.log.info('there was an error during a setup attempt')}
