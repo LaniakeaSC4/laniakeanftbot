5a894b316be4c0053a9657df0df8202cede5ce81
abwglv@gmail.com <abwglv@gmail.com>
2022-09-19 18:04:23 +0100

    New time

--- tools/premium.js
+++ tools/premium.js
@@ -1,65 +1,30 @@
 const w = require('./winston.js')
 const sql = require('./commonSQL.js')//common sql related commands are in here
-/**
- * Adds time to a date. Modelled after MySQL DATE_ADD function.
- * Example: dateAdd(new Date(), 'minute', 30)  //returns 30 minutes from now.
- * https://stackoverflow.com/a/1214753/18511
- * 
- * @param date  Date to start with
- * @param interval  One of: year, quarter, month, week, day, hour, minute, second
- * @param units  Number of units of the given interval to add.
- */
-function dateAdd(date, interval, units) {
-  if(!(date instanceof Date))
-    return undefined;
-  var ret = new Date(date); //don't change original date
-  var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
-  switch(String(interval).toLowerCase()) {
-    case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
-    case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
-    case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
-    case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
-    case 'day'    :  ret.setDate(ret.getDate() + units);  break;
-    case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
-    case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
-    case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
-    default       :  ret = undefined;  break;
-  }
-  return ret;
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
 }
 
 
-
-
 async function updatePremium(serverid, days){
-  var premiumExpire = await sql.getPremiumExpiry(serverid)
-  w.log.info('premiumexpire is:' + premiumExpire[0])
-  if (premiumExpire) {
-    
+  var sqlpremium = await sql.getPremiumExpiry(serverid)
+  w.log.info('sqlpremium is:' + sqlpremium)
+  if (sqlpremium) {
     w.log.info('There was an exisiting expiry time')
-    var oldexpiry = new Date()
-    var newexpiry = new Date()
-    oldexpiry.setDate(premiumExpire)
- w.log.info('old expiry time is: ' + oldexpiry)
-    newexpiry.setDate(oldexpiry + days)
-    w.log.info('new expiry time is: ' + newexpiry)
-    await sql.updateTableColumn('servers', 'serverid', serverid, 'premiumexpire', newexpiry)
+    var premiumexpire = new Date()
+    premiumexpire.setDate(premiumExpire.getDate() + days).toISOString()
+    w.log.info('premiumexpire is: ' + premiumexpire) 
+//await sql.updateTableColumn('servers', 'serverid', serverid, 'premiumexpire', newexpiry)
   
     
   } else {
     w.log.info('There was no existing expiry time')
-    
-
-var date = new Date();
-date.setDate(date.getDate() + 7); 
-
-
-const expirydate = (date.toISOString().split('T')[0])
-
-    
-    
-    w.log.info('expiry time is: ' + expirydate)
-    await sql.updateTableColumn('servers', 'serverid', serverid, 'premiumexpire', expirydate)
+    var premiumexpire = new Date()
+    premiumexpire.setDate(premiumExpire.getDate() + days).toISOString()
+    w.log.info('premiumexpire is: ' + premiumexpire)
+    //await sql.updateTableColumn('servers', 'serverid', serverid, 'premiumexpire', expirydate)
   }
   
 } module.exports.update = updatePremium
