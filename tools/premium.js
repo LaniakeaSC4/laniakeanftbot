adcbd468f9133b7870a89a4d456616e194de677a
andy2004@gmail.com <andy2004@gmail.com>
2022-09-23 06:37:40 +0100

    Ff

--- tools/premium.js
+++ tools/premium.js
@@ -1,6 +1,11 @@
 const w = require('./winston.js')
 const sql = require('./commonSQL.js')//common sql related commands are in here
 
+Date.prototype.addDays = function(d) {
+  this.setTime(this.getTime() + (d*24*60*60*1000));
+  return this;
+}
+
 //function to get (or add) an expiry time to sql database
 async function updatePremium(serverid, days, interaction) {
   if (days > 0) {
@@ -9,16 +14,16 @@ async function updatePremium(serverid, days, interaction) {
       var oldtime = new Date(sqltime)//turn ISO8601 into JS Date
       var add = new Date()//case add as date to be used for calculations
       var newtime = new Date()
-      add.setDay(oldtime.getDay() + days)//add days to exisiting premiumexpire
-      newtime = add.toISOString()//convert to ISO string to save in sql
+      oldtime.addDays(days)//add days to exisiting premiumexpire
+      newtime = oldtime.toISOString()//convert to ISO string to save in sql
       w.log.info("Updating premium expiry for " + serverid + ". There was an exisiting expiry time " + sqltime + " and it has been updated to " + newtime)
       await sql.updateTableColumn("servers", "serverid", serverid, "premiumexpire", newtime)//save
       interaction.reply({ content: "Updating premium expiry for " + serverid + ". There was an exisiting expiry time " + sqltime + " and it has been updated to " + newtime, ephemeral: true })
     } else {//if there wasnt an exisiting time, establish one
       var now = new Date()//get todays date
       var add = new Date()//cast var to be used for calculations
-      add.setDay(now.getDay() + days)//add x days to today
-      var premiumexpire = add.toISOString()//convert to ISO string to save in SQL
+      now.addDays(days)//add x days to today
+      var premiumexpire = now.toISOString()//convert to ISO string to save in SQL
       w.log.info("Updating premium expiry for " + serverid + ". There was not an exisiting expiry time, so expiry has been set to today plus " + days + " days (" + premiumexpire + ")")
       await sql.updateTableColumn("servers", "serverid", serverid, "premiumexpire", premiumexpire)
       interaction.reply({ content: "Updating premium expiry for " + serverid + ". There was not an exisiting expiry time, so expiry has been set to today plus " + days + " days (" + premiumexpire + ")", ephemeral: true })
