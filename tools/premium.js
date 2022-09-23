88d0e4fc7f365a5e9f115c3572669e2fb0501f86
andy2004@gmail.com <andy2004@gmail.com>
2022-09-23 07:00:39 +0100

    Simplify time

--- tools/premium.js
+++ tools/premium.js
@@ -11,23 +11,17 @@ async function updatePremium(serverid, days, interaction) {
   if (days > 0) {
     var sqltime = await sql.getPremiumExpiry(serverid)
     w.log.info(sqltime)
-    if (sqltime != "null") {//there was an exisiting time
-      var oldtime = new Date(sqltime)//turn ISO8601 into JS Date
-      var add = new Date()//case add as date to be used for calculations
-      var newtime = new Date()
-      oldtime.addDays(days)//add days to exisiting premiumexpire
-      newtime = oldtime.toISOString()//convert to ISO string to save in sql
+    if (sqltime != "null") {//there was an exisiting time (sql function makes null result a string)
+      var newtime = new Date(sqltime)
+      newtime.addDays(days)//add days to exisiting premiumexpire
+      newtime.toISOString()//convert to ISO string to save in sql
       w.log.info("Updating premium expiry for " + serverid + ". There was an exisiting expiry time " + sqltime + " and it has been updated to " + newtime)
       await sql.updateTableColumn("servers", "serverid", serverid, "premiumexpire", newtime)//save
       interaction.reply({ content: "Updating premium expiry for " + serverid + ". There was an exisiting expiry time " + sqltime + " and it has been updated to " + newtime, ephemeral: true })
     } else {//if there wasnt an exisiting time, establish one
-    w.log.info('1')
-      var now = new Date()//get todays date
-      w.log.info('2')
-      now.addDays(days)//add x days to today
-      w.log.info('3')
-      var premiumexpire = now.toISOString()//convert to ISO string to save in SQL
-      w.log.info('4')
+      var premiumexpire = new Date()//get todays date
+      premiumexpire.addDays(days)//add x days to today
+      premiumexpire.toISOString()//convert to ISO string to save in SQL
       w.log.info("Updating premium expiry for " + serverid + ". There was not an exisiting expiry time, so expiry has been set to today plus " + days + " days (" + premiumexpire + ")")
       w.log.info('5')
       await sql.updateTableColumn("servers", "serverid", serverid, "premiumexpire", premiumexpire)
