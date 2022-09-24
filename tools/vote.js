f0bfaead17353cadd35000d94b9bb2bb69e8612c
andy2004@gmail.com <andy2004@gmail.com>
2022-09-24 23:07:24 +0100

    Log data

--- tools/vote.js
+++ tools/vote.js
@@ -94,6 +94,7 @@ async function validateCollection(interaction, updown) {
 			});
 			// The whole response has been received. Print out the result.
 			resp.on('end', async () => {
+			  w.log.info(data)
 				if (data.toString().includes("collection not found") === false) {
 				  		//register vote for meslug
 		          await addVote(interaction.message.guildId, interaction.member.user.id, "up", meslug)
