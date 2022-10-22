9425599e3e31e941b4bf6be001ac3a1b1670bd7a
andy2004@gmail.com <andy2004@gmail.com>
2022-10-22 08:47:39 +0100

    Typeof

--- tools/collectionAverage.js
+++ tools/collectionAverage.js
@@ -43,8 +43,9 @@ for (i = 0;i < collections.length;i++){
   if (soloutput.length > 1 && fpoutput.length > 1) {
     var lastsolentry = parseFloat(soloutput.length)
     var secondlastsol = lastsolentry - 1
+    w.log.info(typeof lastsolentry + typeof secondlastsol)
     solchange = soloutput[secondlastsol] / soloutput[lastsolentry]
-    w.log.info('solchange is: ' + solchange)
+    w.log.info('solchange is: ' + solchange + typeof solchange)
     
     var lastfpentry = parseFloat(fpoutput.length)
     var secondlastfp = lastfpentry - 1
