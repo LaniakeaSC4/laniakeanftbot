0e2d5d889cb315577cad7a0c1ebeb3b3a9108fe8
andy2004@gmail.com <andy2004@gmail.com>
2022-10-25 22:51:17 +0100

    Get traits

--- ether/ethertest.js
+++ ether/ethertest.js
@@ -9,7 +9,6 @@ async function ethertest() {
   provider.connection.headers = { "x-qn-api-version": 1 };
   const heads = await provider.send("qn_fetchNFTsByCollection", {
     collection: "0x60E4d786628Fea6478F785A6d7e704777c86a7c6",
-    omitFields: ["imageUrl", "traits"],
     page: 1,
     perPage: 10,
   });
