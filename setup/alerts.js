4eec590b7c1ef8a88ef1ef0d348194962e5f48f3
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-10-27 20:48:57 +0100

    replytext

--- setup/alerts.js
+++ setup/alerts.js
@@ -24,10 +24,40 @@ async function configPanel(interaction) {
         .setStyle(ButtonStyle.Primary),
     )
   //send the reply (including button row)
-  await interaction.reply({ content: "Enable or disable alerts?", components: [row], ephemeral: true })
+
+  //get current config for this server
+  var pingrole = await sql.getData("servers", "serverid", interaction.message.guildId, "pingrole")
+  var ping_enabled = await sql.getData("servers", "serverid", interaction.message.guildId, "enable_ping")
+
+  var replytext = ""
+  if (ping_enabled === true) {
+    if (pingrole) {//enabled and existing. Check if role still exists and confirm back to the user that all is good
+
+      replytext = "Alerts are already enabled for this server. The Role ID is: " + pingrole
+
+    } else {//make a new pingrole. Somehow DB is blank?
+
+      replytext = "Athough alerts are enabled, there was no saved role in our database. Please press [Enable Alerts] again"
+
+    }//end else pingrole was enabled but no role existed.
+
+  } else {//if pingrole not enabled
+    if (pingrole) {//wasn't enabled, but there was a previous pingrole. Check if that role still exists, renable pings and respond to the user confirming the role
+
+      replytext = "Alerts is not currently enabled. There was a previous Alert Role though: " + pingrole
+
+    } else {//there wasn't an exisiting pingrole. Make one and enable pingrolez
+
+      replytext = "Alerts are disabled and there is no exisitng config. Press [Enable Alerts] to get started."
+
+    }//end else pingrole wasn't enabled and role didn't exist
+  }//end else pingrole not enabled
+
+  await interaction.reply({ content: replytext + "\n\nEnable or disable alerts?", components: [row], ephemeral: true })
 } module.exports.configPanel = configPanel
 
 async function enableAlerts(interaction) {
+  //let's get the fresh data again
   var pingrole = await sql.getData("servers", "serverid", interaction.message.guildId, "pingrole")
   var ping_enabled = await sql.getData("servers", "serverid", interaction.message.guildId, "enable_ping")
 
