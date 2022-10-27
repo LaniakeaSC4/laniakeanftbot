0a1178d098866fa368bbac216e9648049f607eed
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-10-27 21:21:41 +0100

    try then

--- setup/alerts.js
+++ setup/alerts.js
@@ -65,10 +65,13 @@ async function enableAlerts(interaction) {
     if (pingrole) {//enabled and existing. Check if role still exists and confirm back to the user that all is good
 
     } else {//make a new pingrole. Somehow DB is blank?
-      var newroleID = await createRole(interaction.message.guildId)
-      w.log.info('EnableAlerts1: New role ID is: ' + newroleID)
-      await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "pingrole", newroleID)
-      await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "enable_ping", true)
+
+      await createRole(interaction.message.guildId).then(async newrole => {
+        w.log.info('EnableAlerts1: New role ID is: ' + newrole)
+        await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "pingrole", newrole)
+        await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "enable_ping", true)
+
+      })
 
     }//end else pingrole was enabled but no role existed.
 
@@ -78,12 +81,12 @@ async function enableAlerts(interaction) {
 
     } else {//there wasn't an exisiting pingrole. Make one and enable pingrolez
 
-      var newroleID = await createRole(interaction.message.guildId)
-      w.log.info('EnableAlerts2: New role ID is: ' + newroleID)
-      await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "pingrole", newroleID)
-      await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "enable_ping", true)
+      await createRole(interaction.message.guildId).then(async newrole => {
+        w.log.info('EnableAlerts2: New role ID is: ' + newrole)
+        await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "pingrole", newrole)
+        await sql.updateTableColumn("servers", "serverid", interaction.message.guildId, "enable_ping", true)
 
-
+      })
 
     }//end else pingrole wasn't enabled and role didn't exist
   }//end else pingrole not enabled
