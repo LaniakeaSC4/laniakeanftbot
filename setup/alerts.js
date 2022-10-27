74df2066ba0fa2db47085a4a94bd96def074c9bb
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2022-10-27 22:28:34 +0100

    get oldrole

--- setup/alerts.js
+++ setup/alerts.js
@@ -35,7 +35,7 @@ async function configPanel(interaction) {
 
       //check guild roles to see if it's still there
       await interaction.message.guild.roles.fetch()
-      let oldrole = await interaction.message.guild.roles.cache.find(x => x.id === pingrole);
+      let oldrole = await interaction.message.guild.roles.cache.get(pingrole)
       w.log.info('oldrole is: ' + oldrole)
       if (typeof oldrole === undefined) {
         // Role doesn't exist, safe to create
