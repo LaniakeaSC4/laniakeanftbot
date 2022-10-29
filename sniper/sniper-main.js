b026e3d74c7a6448b1f6f02409c03469303815e8
andy2004@gmail.com <andy2004@gmail.com>
2022-10-29 15:29:58 +0100

    Hh

--- sniper/sniper-main.js
+++ sniper/sniper-main.js
@@ -183,29 +183,29 @@ async function testifsnipe(raritydescription, thisprice, floorprice) {
 
       if ((raritydescription === 'Mythic') && (thisprice <= mythicsnipe)) {
         
-        /*
+        
         //check if this mythic is within 20% of FP. If so, send alert.
         var alertlimit = floorprice + ((mythicsnipe - floorprice) * 0.2)
         if (thisprice <= alertlimit) {
           resolve([raritydescription, mythicsnipe, mythiclimit, true])
         } else {//not within 20%. No alert.
         resolve([raritydescription, mythicsnipe, mythiclimit, false])
-        } */
+        } 
         
-        resolve([raritydescription, mythicsnipe, mythiclimit, false])
+        //resolve([raritydescription, mythicsnipe, mythiclimit, false])
         
       } else if ((raritydescription === 'Legendary') && (thisprice <= legendarysnipe)) {
         
-        /*
+        
         //check if this mythic is within 5% of FP. If so, send alert.
         var alertlimit = floorprice + ((legendarysnipe - floorprice) * 0.05)
         if (thisprice <= alertlimit) {
           resolve([raritydescription, legendarysnipe, legendarylimit, true])
         } else {//not within 5%. No alert.
         resolve([raritydescription, legendarysnipe, legendarylimit, false])
-        } */
+        } 
         
-        resolve([raritydescription, legendarysnipe, legendarylimit, false])
+        //resolve([raritydescription, legendarysnipe, legendarylimit, false])
 
 
       } else if ((raritydescription === 'Epic') && (thisprice <= epicsnipe)) {
